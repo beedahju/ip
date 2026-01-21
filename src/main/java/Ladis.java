e8a381b68b2a024edf31decb4278c6724c9b3b93
beedahju <judahpang@gmail.com>
2026-01-22 00:22:15 +0800

    Echo

--- src/main/java/Ladis.java
+++ src/main/java/Ladis.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Ladis {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Ladis\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        
+        while (true) {
+            String user_input = sc.nextLine().trim().toLowerCase();
+
+            if (user_input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println("    " + user_input);
+            System.out.println("____________________________________________________________");
+        }
+        sc.close();
     }
 }
