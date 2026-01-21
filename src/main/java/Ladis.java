85825d440c78afae9af2d66d6715d78bb3adc547
beedahju <judahpang@gmail.com>
2026-01-22 00:09:06 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Ladis.java
@@ -0,0 +1,8 @@
+public class Duke {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Ladis\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
