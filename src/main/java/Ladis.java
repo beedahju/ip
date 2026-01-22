9d7db62c1d79117c07fd42d7c5217ccbaad62160
beedahju <judahpang@gmail.com>
2026-01-22 13:07:34 +0800

    Delete

--- src/main/java/Ladis.java
+++ src/main/java/Ladis.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ladis {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Task[] user_inputs = new Task[100]; 
-        int input_count = 0;
+        ArrayList<Task> user_inputs = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Ladis\nWhat can I do for you?");
@@ -23,8 +23,8 @@ public class Ladis {
 
                 if (user_input.equals("list")) {
                     System.out.println("____________________________________________________________");
-                    for (int i = 0; i < input_count; i++) {
-                        System.out.println((i + 1) + ". " + user_inputs[i]);
+                    for (int i = 0; i < user_inputs.size(); i++) {
+                        System.out.println((i + 1) + ". " + user_inputs.get(i));
                     }
                     System.out.println("____________________________________________________________");
                     continue;
@@ -33,13 +33,13 @@ public class Ladis {
                 if (user_input.startsWith("mark")) {
                     try{
                         int index = Integer.parseInt(user_input.substring(5)) - 1;
-                        if (index < 0 || index >= input_count) {
+                        if (index < 0 || index >= user_inputs.size()) {
                             throw new LadisExeception("Very funny. Now give me a valid task number.");
                         }
-                        user_inputs[index].mark();
+                        user_inputs.get(index).mark();
                         System.out.println("____________________________________________________________");
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("    " + user_inputs[index]);
+                        System.out.println("    " + user_inputs.get(index));
                         continue;
                     } catch (NumberFormatException e) {
                         throw new LadisExeception("Very funny. Now give me a valid task number.");
@@ -49,13 +49,13 @@ public class Ladis {
                 if (user_input.startsWith("unmark")) {
                     try{
                         int index = Integer.parseInt(user_input.substring(7)) - 1;
-                        if (index < 0 || index >= input_count) {
+                        if (index < 0 || index >= user_inputs.size()) {
                             throw new LadisExeception("Very funny. Now give me a valid task number.");
                         }
-                        user_inputs[index].unmark();
+                        user_inputs.get(index).unmark();
                         System.out.println("____________________________________________________________");
                         System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println("    " + user_inputs[index]);
+                        System.out.println("    " + user_inputs.get(index));
                         continue;
                     } catch (NumberFormatException e) {
                         throw new LadisExeception("Very funny. Now give me a valid task number.");
@@ -67,12 +67,12 @@ public class Ladis {
                         throw new LadisExeception("Please don't be funny and give me a proper description.");
                     }
                     String description = user_input.substring(5);
-                    user_inputs[input_count++] = new Todo(description);
+                    user_inputs.add(new Todo(description));
 
                     System.out.println("____________________________________________________________");
                     System.out.println("Got it. I've added this task:");
                     System.out.println("  [T][ ] " + description);
-                    System.out.println("Now you have " + input_count + " tasks in the list.");
+                    System.out.println("Now you have " + user_inputs.size() + " tasks in the list.");
                     System.out.println("____________________________________________________________");
                     continue;
                 }
@@ -83,11 +83,11 @@ public class Ladis {
                     }
 
                     String[] parts = user_input.substring(9).split(" /by ");
-                    user_inputs[input_count++] = new Deadline(parts[0], parts[1]);
+                    user_inputs.add(new Deadline(parts[0], parts[1]));
                     System.out.println("____________________________________________________________");
                     System.out.println("Got it. I've added this task:");
                     System.out.println("  [D][ ] " + parts[0] + " (by: " + parts[1] + ")");
-                    System.out.println("Now you have " + input_count + " tasks in the list.");
+                    System.out.println("Now you have " + user_inputs.size() + " tasks in the list.");
                     System.out.println("____________________________________________________________");
                     continue;
                 }
@@ -97,14 +97,35 @@ public class Ladis {
                         throw new LadisExeception("Interesting event. When does it start and end?");
                     }
                     String[] parts = user_input.substring(6).split(" /from | /to ");
-                    user_inputs[input_count++] = new Event(parts[0], parts[1], parts[2]);
+                    user_inputs.add(new Event(parts[0], parts[1], parts[2]));
                     System.out.println("____________________________________________________________");
                     System.out.println("Got it. I've added this task:");
                     System.out.println("  [E][ ] " + parts[0] +
                             " (from: " + parts[1] + " to: " + parts[2] + ")");
-                    System.out.println("Now you have " + input_count + " tasks in the list.");
+                    System.out.println("Now you have " + user_inputs.size() + " tasks in the list.");
                     System.out.println("____________________________________________________________");
                 }
+
+                if (user_input.startsWith("delete")) {
+                    try {
+                        int index = Integer.parseInt(user_input.substring(7)) - 1;
+
+                        if (index < 0 || index >= user_inputs.size()) {
+                            throw new LadisExeception("Very funny. Now give me a valid task number.");
+                        }
+
+                        Task removed = user_inputs.remove(index);
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Noted. I've removed this task:");
+                        System.out.println("  " + removed);
+                        System.out.println("Now you have " + user_inputs.size() + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                        continue;
+
+                    } catch (NumberFormatException e) {
+                        throw new LadisExeception("Please provide a valid task number.");
+                    }
+            }       
                 throw new LadisExeception("I'm sorry, but I don't know what that means :-(");
             }
                 catch(LadisExeception e) {
